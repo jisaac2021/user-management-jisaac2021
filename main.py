c23cfea6c63dc301e06c26696c9771770f1a25d0
jisaac2021 <jisaac2021@kehillahstudent.org>
2018-12-26 20:48:14 -0800

    create sqlite3 database

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from tkinter import *
+from tkinter import messagebox
 import os
+import sqlite3
+
+conn = sqlite3.connect("userinfo.db")
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS user(username TEXT,password TEXT)")
+c.execute("SELECT * FROM user")
+conn.commit()
 
 def delete1():
     screen1.destroy()
+
+def delete2():
+    screen2.destroy()
 
 def register_user():
     username_info = username.get()
     password_info = password.get()
 
-    with open('userinfo.txt',"w") as file:
+    find_user = ("SELECT * FROM user WHERE username = ?")
+    c.execute(find_user,[(username.get())])
+    if c.fetchall():
+        messagebox.showerror("Error")
+    else:
+        messagebox.showinfo("Success")
+    params = (username_info, password_info)
+    c.execute('INSERT INTO user VALUES(?,?)', params)
+    conn.commit()
 
-        file.write(username_info+"\n")
-        file.write(password_info+"\n")
 
-    username_entry.delete(0, END)
-    password_entry.delete(0, END)
+    """with open('userinfo.txt',"w") as file:
+
+        file.write(username_info)
+        file.write(",")
+        file.write(password_info)
+        file.write("\n")"""
 
     Label(screen1, text="Success").pack()
     Button(screen1, text="OK", command=delete1).pack()
@@ -36,28 +57,22 @@ def register():
     Label(screen1, text = "").pack()
     Label(screen1, text = "Username * ").pack()
 
-    username_entry = Entry(screen1, textvariable = username)
-    username_entry.pack()
+    username_entry = Entry(screen1, textvariable = username).pack()
     Label(screen1, text = "Password * ").pack()
-    password_entry =  Entry(screen1, textvariable = password)
-    password_entry.pack()
+    password_entry =  Entry(screen1, textvariable = password).pack()
     Label(screen1, text = "").pack()
     Button(screen1, text = "Register", width = 10, height = 1, command = register_user).pack()
-
-def login():
-    print('Test')
 
 def main():
     global wn
     wn = Tk()
     wn.geometry("300x250")
     wn.title("User Management")
-    Label(text = "User Management", bg = "grey", width = "300", height = "2", font = ("Inconsolata", 13)).pack()
-    Label(text = "").pack()
+
+    head = Label(text = "User Management", bg = "grey", width = "300", height = "2", font = ("freesansbold", 13), pady=40).pack()
+    logf = Frame(padx=10,pady=10)
     Button(text = "Login", height = "2", width = "30", command = login).pack()
-    Label(text = "").pack()
     Button(text = "Register", height = "2", width = "30", command = register).pack()
-    Label(text = "").pack()
     wn.mainloop()
 
 main()
